de2019985a3366537a413928e7eaea79055c4dfb
krizhanovskiy.a <krizhanovskiy.a@ipland.com.ua>
2019-01-02 15:30:32 +0200

    --ADD Heroku Dependenciess

--- src/main/java/vastokbot/VastokBot.java
+++ src/main/java/vastokbot/VastokBot.java
@@ -130,6 +130,7 @@ public class VastokBot extends TelegramLongPollingBot {
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
+
             }
         }
     }
